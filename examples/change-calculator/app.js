f710efb0980efbead23f7629556eefa12fa9a11e
Ron Waldon <jokeyrhyme@gmail.com>
2014-02-04 10:11:06 +1100

    change-calculator: pass 2 - initial JavaScript

--- examples/change-calculator/app.js
+++ examples/change-calculator/app.js
@@ -2,7 +2,7 @@
 
 /*globals angular*/ // Angular.JS
 
-/*globals calculateChange*/ // our new function
+/*globals calculateChange, round005*/ // our new functions
 
 var app = angular.module('app', ['ng']);
 
@@ -14,31 +14,17 @@ function round(value, decimals) {
   return value;
 }
 
-function round5(value) {
-  var coefficient, remainder;
-  coefficient = 100;
-  value *= coefficient;
-  remainder = value % 5;
-  if (remainder < 5) {
-    value -= remainder;
-  } else {
-    value += 10 - remainder;
-  }
-  value /= coefficient;
-  return value;
-}
-
 app.controller('ChangeCtrl', [
   '$scope',
   function ($scope) {
-    $scope.units = calculateChange.UNITS;
     $scope.price = Math.random() * 100;
     $scope.cash = 100;
     $scope.update = function () {
       $scope.price = round($scope.price, 2);
       $scope.cash = round($scope.cash, 2);
       $scope.difference = round($scope.cash - $scope.price, 2);
-      $scope.rounded = round5($scope.difference);
+      $scope.rounded = round005($scope.difference);
+      $scope.change = calculateChange($scope.price, $scope.cash);
     };
     $scope.update();
   }
--- examples/change-calculator/calc.js
+++ examples/change-calculator/calc.js
@@ -1,7 +1,33 @@
 /*jslint indent:2*/
 
-function calculateChange() {
+function round005(value) {
+  var remainder;
+  value *= 100;
+  remainder = value % 5;
+  if (remainder < 5) {
+    value -= remainder;
+  } else {
+    value += 10 - remainder;
+  }
+  value /= 100;
+  return value;
 }
 
-calculateChange.UNITS = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05];
+function calculateChange(price, cash) {
+  var difference, change, units;
+  units = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05];
+  difference = cash - price;
+  difference = round005(difference);
+  change = {};
+  units.forEach(function (unit) {
+    while (unit < difference) {
+      if (!change[unit]) {
+        change[unit] = 0;
+      }
+      change[unit] += 1;
+      difference -= unit;
+    }
+  });
+  return change;
+}
 
